db51308055e1361a5ff1b465500c5e316588799f
micder1 <tuliopalacine@hotmail.com>
2022-11-28 19:29:13 -0300

    perfil aluno

--- codigo/js/perfil-aluno.js
+++ codigo/js/perfil-aluno.js
@@ -13,3 +13,83 @@ if (!checaLogin()) {
   alert("Você não pode acessar a página de outro aluno");
   window.location.href = "./index.html";
 }
+
+function setDadosAluno(){
+  const nomeCompleto = uLogado.nome + " " + uLogado.sobrenome;
+  console.log(nomeCompleto)
+
+  document.querySelector('.descricao-aluno').innerHTML = `
+    <h4>${nomeCompleto}</h4>
+    <p class="text-secondary mb-1">${uLogado.categoria}</p>
+    <p class="text-muted font-size-sm">${uLogado.email}</p>
+  `
+}
+
+setDadosAluno()
+
+const aulas = leAulas();
+const user = leUsers();
+
+function atualizaAulas(){
+
+  for(let i = 0 ; i < aulas.length ; i++){
+    if(aulas[i].idAluno == uLogado.id){
+      if(aulas[i].status == 'aguardando'){
+        const divAula = document.createElement("div")
+        divAula.className = "d-flex mt-2 p-2 info-professor"
+        divAula.id = `aula-${aulas.id}`
+        
+        console.log(aulas[i].idProfessor)
+        console.log(user[aulas[i].idProfessor -1].nome)
+
+        document.querySelector(`.aula-aguardando`).innerHTML += `
+        <div class="justify-content-start">
+          <i class="pr-1 fas fa-user text-muted"></i> 
+            Professor(a) ${user[aulas[i].idProfessor -1].nome}
+            <p>Código da aula: ${aulas[i].id} </p>
+          </div>
+        <div class="ml-auto opcoes-professor">
+        </div>
+        `
+      }
+      else if(aulas[i].status == 'em andamento'){
+        const divAula = document.createElement("div")
+        divAula.className = "d-flex mt-2 p-2 info-professor"
+        divAula.id = `aula-${aulas.id}`
+        
+        console.log(aulas[i].idProfessor)
+        console.log(user[aulas[i].idProfessor -1].nome)
+
+        document.querySelector(`.aula-andamento`).innerHTML += `
+        <div class="justify-content-start">
+          <i class="pr-1 fas fa-user text-muted"></i> 
+            Professor ${user[aulas[i].idProfessor -1].nome}
+            <p>Código da aula: ${aulas[i].id} </p>
+          </div>
+        <div class="ml-auto opcoes-professor">
+        </div>
+        `
+      }
+      else if(aulas[i].status == 'concluida'){
+        const divAula = document.createElement("div")
+        divAula.className = "d-flex mt-2 p-2 info-professor"
+        divAula.id = `aula-${aulas.id}`
+        
+        console.log(aulas[i].idProfessor)
+        console.log(user[aulas[i].idProfessor -1].nome)
+
+        document.querySelector(`.aula-finalizada`).innerHTML += `
+        <div class="justify-content-start">
+          <i class="pr-1 fas fa-user text-muted"></i> 
+            Professor ${user[aulas[i].idProfessor -1].nome}
+            <p>Código da aula: ${aulas[i].id} </p>
+          </div>
+        <div class="ml-auto opcoes-professor">
+        </div>
+        `
+      }
+    }
+  }
+}
+
+atualizaAulas()
